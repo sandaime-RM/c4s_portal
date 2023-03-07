583849b599e22d089c7763e194c6ca0617bc4460
sandaime-RM <sienryu1129@outlook.jp>
2023-03-07 22:16:11 +0900

    修正

--- script/login.js
+++ script/login.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
-import { GoogleAuthProvider, getAuth } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+import { GoogleAuthProvider, getAuth, getRedirectResult } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 

bce679d1058cf8a58709cbbaaeeacbede18d2a45
sandaime-RM <sienryu1129@outlook.jp>
2023-03-07 22:09:05 +0900

    とりあえずアップ

---
+++ script/login.js
@@ -0,0 +1,52 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
+import { GoogleAuthProvider, getAuth } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBE60G8yImWlENWpCnQZzqqVUrwWa_torg",
+    authDomain: "c4s-portal.firebaseapp.com",
+    databaseURL: "https://c4s-portal-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "c4s-portal",
+    storageBucket: "c4s-portal.appspot.com",
+    messagingSenderId: "863775995414",
+    appId: "1:863775995414:web:82eb9557a13a099dfbe737",
+    measurementId: "G-K2SR1WSNRC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth();
+
+//ログイン
+function login() {
+    getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+  
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+window.login = login;
+export{login}

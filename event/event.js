54fc6a3998ba7d449484edbef0a0749a68830d02
sandaime-RM <sienryu1129@outlook.jp>
2023-04-20 23:11:43 +0900

    背景色変更

---
+++ event/event.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+import { getDatabase, ref, onChildAdded, push, remove, set, get } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBE60G8yImWlENWpCnQZzqqVUrwWa_torg",
+    authDomain: "c4s-portal.firebaseapp.com",
+    databaseURL: "https://c4s-portal-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "c4s-portal",
+    storageBucket: "c4s-portal.appspot.com",
+    messagingSenderId: "863775995414",
+    appId: "1:863775995414:web:82eb9557a13a099dfbe737",
+    measurementId: "G-K2SR1WSNRC"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+const auth = getAuth();
+
+//ユーザー情報の取得
+onAuthStateChanged(auth, (us) => {
+    user = us;
+});

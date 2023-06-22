0fe8e041640a1004ab86b00f0a7b5bd656f49635
toyton22 <toyton.niscev@gmail.com>
2023-06-22 11:10:40 +0900

    メソッド使ってみようとした

--- script/methods.js
+++ script/methods.js
@@ -1,8 +1,8 @@
 export function getObj(id) { return document.getElementById(id); }
 export function hide(id) { getObj(id).style.display = "none"; }
 export function show(id) { getObj(id).style.display = "block"; }
-export function addhead(id, HTML) { getObj(id).innerHTML += HTML; }
-export function addtail(id, HTML) { getObj(id).innerHTML = HTML + getObj(id).innerHTML; }
+export function addhead(id, HTML) { getObj(id).innerHTML = HTML + getObj(id).innerHTML; }
+export function addtail(id, HTML) { getObj(id).innerHTML = getObj(id).innerHTML + HTML; }
 
 //部員をソート
 export function sortMembers(users, keys){

0ef5c288ee5085cf8e795ab98a55c31695efd193
sandaime-RM <sienryu1129@outlook.jp>
2023-06-22 15:01:35 +0900

    予約機能完成

--- room/room.js
+++ room/room.js
@@ -26,3 +26,110 @@ const analytics = getAnalytics(app);
 const db = getDatabase(app);
 const auth = getAuth();
 var user;
+var roomData;
+var roomIds = [];
+var editting = -1; //編集中の予約情報（-1の場合はなし）
+
+//HTML要素
+var bookList = document.getElementById("bookList");
+var editModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+
+
+//ユーザー情報の取得
+onAuthStateChanged(auth, (us) => {
+    user = us;
+
+    //予約状況の取得
+    get(ref(db, "rooms")).then((snapshot) => {
+        roomData = snapshot.val();
+
+        //予約データを１つずつ表示
+        Object.keys(roomData).forEach((key, index) => {
+            var room = roomData[key];
+            var rdate = new Date(room.date);
+            var date = ('0' + (rdate.getMonth() + 1)).slice(-2) + "/" + ('0' + (rdate.getDate())).slice(-2);
+            var status = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="my-1 bi bi-dash-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/></svg><div class="small">申請中</div>';
+            var color = "secondary";
+
+            //状態表示
+            if(room.status == 1) {
+                color = "success"
+                status = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="my-1 bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg><div class="small">予約済</div>';
+            }
+
+            if(room.status == 2) {
+                color = "danger"
+                status = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="my-1 bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg><div class="small">不可</div>';
+            }
+
+            //表示
+            bookList.innerHTML = '<li class="list-group-item" style="cursor: pointer;" onclick="edit('+index+')"><div class="row"><div class="col-4"><div class="fs-5 fw-bold">'+date+'</div><div class="small">'+room.start+'～'+room.end+'</div></div><div class="col-5"><div class="fs-5 fw-bold">'+room.building+'</div><div>'+room.roomNum+'</div></div><div class="col-3 text-'+color+' text-center">'+status+'</div></div></li>' + bookList.innerHTML;
+
+            roomIds.push(key);
+        });
+    });
+});
+
+//アップロード
+function upload() {
+    if(editting == -1) {
+        push(ref(db, "rooms"), {
+            date : (new Date(document.getElementById("rdate").value)).getTime(),
+            start : document.getElementById("rstart").value,
+            end : document.getElementById("rend").value,
+            building : document.getElementById("rbuilding").value,
+            roomNum : document.getElementById("rroomNum").value,
+            status : document.getElementById("rstatus").selectedIndex
+        })
+        .then(() => {
+            window.location.reload();
+        });
+    } else {
+        set(ref(db, "rooms/" + roomIds[editting]), {
+            date : (new Date(document.getElementById("rdate").value)).getTime(),
+            start : document.getElementById("rstart").value,
+            end : document.getElementById("rend").value,
+            building : document.getElementById("rbuilding").value,
+            roomNum : document.getElementById("rroomNum").value,
+            status : document.getElementById("rstatus").selectedIndex
+        })
+        .then(() => {
+            window.location.reload();
+        });
+    }
+    
+}
+
+window.upload = upload;
+export{upload}
+
+//編集画面の表示
+function edit(index) {
+    var room = roomData[roomIds[index]];
+    editting = index;
+
+    document.getElementById("rdate").value = (new Date(room.date)).getFullYear() + "-" + ('0' + ((new Date(room.date)).getMonth() + 1)).slice(-2) + "-" + ('0' + (new Date(room.date)).getDate()).slice(-2);
+    document.getElementById("rbuilding").value = room.building;
+    document.getElementById("rroomNum").value = room.roomNum;
+    document.getElementById("rstart").value = room.start;
+    document.getElementById("rend").value = room.end;
+    document.getElementById("rstatus").selectedIndex = room.status;
+    editModal.show();
+}
+
+window.edit = edit;
+export{edit}
+
+
+//編集画面の初期化
+function clearForm() {
+    document.getElementById("rdate").value = "";
+    document.getElementById("rbuilding").value = "講義棟";
+    document.getElementById("rroomNum").value = "K409";
+    document.getElementById("rstart").value = "18:00";
+    document.getElementById("rend").value = "21:00";
+    document.getElementById("rstatus").selectedIndex = 0;
+}
+
+window.clearForm = clearForm;
+export{clearForm}

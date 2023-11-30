209adfc57d3065684e17fdb5f29f04dba5f4ae42
sandaime-RM <sienryu1129@outlook.jp>
2023-11-30 16:35:07 +0900

    メルマガのメール送信機能

--- control/magazine.js
+++ control/magazine.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
-import { getDatabase, ref, update, push, remove, set, get } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+import { getDatabase, ref, update, push, remove, set, get, onValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
 import { getObj, sortMembers } from "/script/methods.js";
 
 const firebaseConfig = {
@@ -21,25 +21,32 @@ const auth = getAuth();
 var user;
 var followers;
 var mails;
+var myName = "";
 
 var adminusers;
 
+//Modal
+const modal = new bootstrap.Modal(document.getElementById('editMail'))
+
 //ユーザー情報の取得
 onAuthStateChanged(auth, (us) => {
-  user = us;
+    user = us;
 
-  //ローディング画面
-  
-  //管理者一覧を取得
-  get(ref(db, "admin-users")).then((snapshot) => {
-    adminusers = snapshot.val();
-    //管理者権限のないゲストはアクセス拒否
-    if(!adminusers[user.uid]) { if(!alert("管理者権限がありません")) { location.href = "/"; }}
-    //でなければアクセス許可
-    else { 
-      loadInfo();
-    }
-  })
+    //ローディング画面
+    
+    //管理者一覧を取得
+    get(ref(db, "admin-users")).then((snapshot) => {
+      adminusers = snapshot.val();
+      //管理者権限のないゲストはアクセス拒否
+      if(!adminusers[user.uid]) { if(!alert("管理者権限がありません")) { location.href = "/"; }}
+      //でなければアクセス許可
+      else { 
+        get(ref(db, "users/" + user.uid + "/name")).then((snapshot) => {
+          myName = snapshot.val();
+          loadInfo();
+        })
+      }
+    })
 });
 
 //登録者とメール一覧の表示
@@ -48,8 +55,10 @@ function loadInfo() {
     $("#overray").fadeOut();
 
     //登録者一覧の取得と表示
-    get(ref(db, "magazine/followers")).then((snapshot) => {
+    onValue(ref(db, "magazine/followers"), (snapshot) => {
         followers = snapshot.val();
+
+        getObj("followerList").innerHTML = "";
 
         Object.keys(followers).forEach((key, index) => {
             getObj("followerList").innerHTML += '<tr><td>'+(index+1)+'</td><td>'+followers[key].name+'</td><td>'+followers[key].email+'</td><td><button class="btn btn-danger py-0" onclick="delFollower('+index+')"><i class="bi bi-trash"></i></button></td></tr>';
@@ -57,11 +66,54 @@ function loadInfo() {
     });
 
     //メール一覧の取得と表示
-    get(ref(db, "magazine/mails")).then((snapshot) => {
-        mails = snapshot.val();
+    onValue(ref(db, "magazine/mails"), (snapshot2) => {
+        mails = snapshot2.val();
+
+        getObj("mailList").innerHTML = "";
 
         Object.keys(mails).forEach((key, index) => {
-            getObj("mailList").innerHTML += '<tr><td>'+mails[key].date+'</td><td>'+mails[key].title+'</td><td>'+mails[key].sender+'</td><td><button class="btn btn-default py-0" onclick="delFollower('+index+')"><i class="bi bi-box-arrow-up-right"></i></button></td></tr>';
+            getObj("mailList").innerHTML = '<tr><td>'+mails[key].date+'</td><td>'+mails[key].title+'</td><td>'+mails[key].sender+'</td><td><button class="btn btn-default py-0" onclick="delFollower('+index+')"><i class="bi bi-box-arrow-up-right"></i></button></td></tr>' + getObj("mailList").innerHTML;
         });
     })
 }
+
+//メールの送信
+function send() {
+    var ask = confirm("メールの配信をしてよろしいですか？内容をよくご確認ください。");
+
+    if(!ask) { return; }
+
+    var date = new Date();
+
+    push(ref(db, "magazine/mails"), {
+        title : getObj("mailTitle").value,
+        content : getObj("mailContent").value,
+        sender : myName,
+        date : date.toLocaleString()
+    })
+    .then(() => {
+      alert("配信しました。");
+      modal.hide();
+    })
+}
+
+window.send = send;
+export{send}
+
+
+//登録者の削除
+function delFollower(num) {
+    var key = Object.keys(followers)[num];
+
+    var ask = confirm(followers[key].name + " さんのメルマガ登録を削除してもよろしいですか？");
+
+    if(!ask) { return; }
+
+    remove(ref(db, "magazine/followers/" + key))
+    .then(() => {
+        console.log("The follower was removed.");
+    })
+}
+
+window.delFollower = delFollower
+export{delFollower}
